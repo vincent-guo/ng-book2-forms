8128ed4b174a0ab66b58bf575c1fbab144c3322d
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-03 17:34:17 +0800

    Create the FormSku component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSkuComponent
   ],
   imports: [
     BrowserModule

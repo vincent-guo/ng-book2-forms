32af083e1a7cd794d41896708e013d4f8f79f185
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-05 16:29:51 +0800

    Modify the FormWithValidationsExplicit component

--- src/app/form-with-validations-explicit/form-with-validations-explicit.component.ts
+++ src/app/form-with-validations-explicit/form-with-validations-explicit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-with-validations-explicit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-with-validations-explicit.component.css']
 })
 export class FormWithValidationsExplicitComponent implements OnInit {
+  myForm: FormGroup;
+  sku: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.required]
+    });
+
+    this.sku = this.myForm.controls['sku'];
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
   }
 
 }

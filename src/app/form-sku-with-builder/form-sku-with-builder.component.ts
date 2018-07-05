f4870f41820c3ad07dfbbae506f47356267bd6a1
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-05 16:11:48 +0800

    Modify the FormSkuWithBuilder component

--- src/app/form-sku-with-builder/form-sku-with-builder.component.ts
+++ src/app/form-sku-with-builder/form-sku-with-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-sku-with-builder',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-sku-with-builder.component.css']
 })
 export class FormSkuWithBuilderComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
   }
 
 }

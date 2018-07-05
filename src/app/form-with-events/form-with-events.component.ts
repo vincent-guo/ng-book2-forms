713137bce3cdb466f41270a89c560ba9a2900c56
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-05 17:35:01 +0800

    Modify the FormWithEvents component

--- src/app/form-with-events/form-with-events.component.ts
+++ src/app/form-with-events/form-with-events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-with-events',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-with-events.component.css']
 })
 export class FormWithEventsComponent implements OnInit {
+  myForm: FormGroup;
+  sku: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.required]
+    });
+
+    this.sku = this.myForm.controls['sku'];
+
+    this.sku.valueChanges.subscribe(
+      (value: string) => {
+        console.log('sku changed to: ', value);
+      }
+    );
+
+    this.myForm.valueChanges.subscribe(
+      (form: any) => {
+        console.log('form changed to: ', form);
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: any): void {
+    console.log('you submitted value:', form.sku);
   }
 
 }
